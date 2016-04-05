650d240fc9f40f0e63c048ef552cd4bae21ca9f8
Adam Castle <acastle@ripe.net>
2016-04-05 19:02:45 +0200

    Krissssss

--- abortretryfail/views.py
+++ abortretryfail/views.py
@@ -9,3 +9,8 @@ def my_view(request):
 @view_config(route_name='maps/amsterdam-ww2', renderer='templates/maps/ww2-amsterdam/template.pt')
 def amsterdam_ww2(request):
     return {'project': 'abortretryfail'}
+
+
+@view_config(route_name='isitfriday', renderer='templates/isitfriday.pt')
+def isitfriday(request):
+    return {'project': 'abortretryfail'}
